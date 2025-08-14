54ff5284f0e2c89a435703f5b2f623bb947d868d
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-08-14 15:06:05 +0300

    Limit The ip Requests

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.set('views', __dirname + '/views');
 app.use(express.urlencoded({ extended: false }));
 
 const limiter = rateLimit({
-    windowMs: 60 * 60 * 1000, // 1 hour
-    max: 100, // limit each IP to 10 requests per windowMs
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 10, // limit each IP to 10 requests per windowMs
     standardHeaders: true,
     legacyHeaders: false,
 });
