e9bdd934964901758ce8af3438068b9d6ff0af17
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-08-14 12:43:20 +0300

    add Global error middleware

--- server.js
+++ server.js
@@ -15,7 +15,13 @@ app.use(express.urlencoded({ extended: false }));
 const urlRoutes = require('./routes/url.routes');
 app.use('/', urlRoutes);
 
-app.listen(PORT,()=> {
+// error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB.connect();
-})
+});
