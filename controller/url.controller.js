2faf83548ff459080b057b6aadd0f2b9bd60e452
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-08-14 12:15:39 +0300

    new version

--- controller/url.controller.js
+++ controller/url.controller.js
@@ -1,26 +1,40 @@
-const ShortUrl = require('../models/shortUrl')
+const ShortUrl = require('../models/shortUrl.model')
+const shortid = require('shortid');
 
-const getall = async (req, res) => {
-    const shortUrls = await ShortUrl.find();
-    res.render('index', { shortUrls: shortUrls });
+
+
+// @desc render the main page
+// @route GET /
+// @access Public
+exports.render = async (req, res) => {
+    res.render('index', { shortUrls: [] });
 }
 
-const create = async (req, res) => {
-    await ShortUrl.create({ longUrl: req.body.longUrl });
-    res.redirect('/');
+//@desc create a short URL
+//@route POST /api/shorturl
+//@access Public
+exports.createUrl = async (req, res) => {
+    const longUrl = req.body.longUrl;
+    const found = await ShortUrl.findOne({ longUrl });
+    if (found) {
+        return res.render('index', { shortUrls: [found] });
+    }
+    const url = await ShortUrl.create({ longUrl: req.body.longUrl });
+    url.shortUrl = `${req.protocol}://${req.get('host')}/${shortid.generate()}`;
+    // console.log(url);
+    await url.save();
+    res.render('index', { shortUrls: [url] });
 }
 
-const redirectToLongUrl = async (req, res) => {
-    const url = req.params.shortUrl;
+//@desc redirect to the long URL
+//@route GET /:shortUrl
+//@access Public
+exports.redirectToLongUrl = async (req, res) => {
+    const url = `${req.protocol}://${req.get('host')}/${req.params.shortUrl}`;
     const foundUrl = await ShortUrl.findOne({ shortUrl: url });
     if (!foundUrl)
         return res.sendStatus(404);
     foundUrl.clicks++;
     await foundUrl.save();
     res.redirect(foundUrl.longUrl);
-}
-module.exports = {
-    getall,
-    create,
-    redirectToLongUrl,
 }
--- routes/url.routes.js
+++ routes/url.routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const Router = express.Router();
 
-const urlcontroller = require('../controller/url.controller');
+const {render , createUrl, redirectToLongUrl} = require('../controller/url.controller');
 
-Router.get('/', urlcontroller.getall);
-Router.post('/shortUrls', urlcontroller.create);
-Router.get('/:shortUrl', urlcontroller.redirectToLongUrl);
+Router.get('/', render);
+Router.post('/', createUrl);
+Router.get('/:shortUrl', redirectToLongUrl);
 
 module.exports = Router;
