4e32851fe580bd72acc0716f732f83fc62f6711e
Mahmoud Abdellah <mahmoud.abdellah014@gmail.com>
2025-08-14 12:48:24 +0300

    Apply asyncWrapper to Controllers

--- controller/url.controller.js
+++ controller/url.controller.js
@@ -1,19 +1,19 @@
 const ShortUrl = require('../models/shortUrl.model')
 const shortid = require('shortid');
-
+const asyncWrapper = require('../middlewares/asyncWrapper');
 
 
 // @desc render the main page
 // @route GET /
 // @access Public
-exports.render = async (req, res) => {
+exports.render = asyncWrapper(async (req, res) => {
     res.render('index', { shortUrls: [] });
-}
+});
 
 //@desc create a short URL
 //@route POST /api/shorturl
 //@access Public
-exports.createUrl = async (req, res) => {
+exports.createUrl = asyncWrapper(async (req, res) => {
     const longUrl = req.body.longUrl;
     const found = await ShortUrl.findOne({ longUrl });
     if (found) {
@@ -24,12 +24,13 @@ exports.createUrl = async (req, res) => {
     // console.log(url);
     await url.save();
     res.render('index', { shortUrls: [url] });
-}
+});
+
 
 //@desc redirect to the long URL
 //@route GET /:shortUrl
 //@access Public
-exports.redirectToLongUrl = async (req, res) => {
+exports.redirectToLongUrl = asyncWrapper(async (req, res) => {
     const url = `${req.protocol}://${req.get('host')}/${req.params.shortUrl}`;
     const foundUrl = await ShortUrl.findOne({ shortUrl: url });
     if (!foundUrl)
@@ -37,4 +38,4 @@ exports.redirectToLongUrl = async (req, res) => {
     foundUrl.clicks++;
     await foundUrl.save();
     res.redirect(foundUrl.longUrl);
-}
+});
